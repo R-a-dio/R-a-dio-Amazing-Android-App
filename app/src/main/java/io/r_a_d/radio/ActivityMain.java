aaae3cdb3444bde8deda0a17cf3411fd62663f6a
Robert Rowe <robrowe5@aol.com>
2016-12-28 19:53:03 -0800

    Add Basic Streaming
    The simplest streaming possible. Will have delay before audio plays
    depending on connection speed. Changes play/pause icons when pressed,
    will add buffering icon at a later time.

--- app/src/main/java/io/r_a_d/radio/ActivityMain.java
+++ app/src/main/java/io/r_a_d/radio/ActivityMain.java
@@ -1,10 +1,20 @@
 package io.r_a_d.radio;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.io.IOException;
 
 public class ActivityMain extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer = null;
+    private boolean playing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +24,44 @@ public class ActivityMain extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new CustomPagerAdapter(this));
 
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(this, Uri.parse("https://stream.r-a-d.io/main.mp3"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+    }
+
+    public void togglePlayPause(View v) throws IOException {
+        ImageButton img = (ImageButton)v.findViewById(R.id.play_pause);
+        if(!playing){
+            img.setImageResource(R.drawable.pause_small);
+            mediaPlayer.prepareAsync();
+            playing = true;
+        } else {
+            img.setImageResource(R.drawable.arrow_small);
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                mediaPlayer.setDataSource(this, Uri.parse("https://stream.r-a-d.io/main.mp3"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+            playing = false;
+        }
     }
 }
