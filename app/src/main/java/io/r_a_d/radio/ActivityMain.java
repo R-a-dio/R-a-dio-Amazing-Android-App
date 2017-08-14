532e64bd754f772e15936e63c3419d39a6619f0b
Kethsar <Kethprall@gmail.com>
2017-08-13 18:38:05 -0700

    Cleanup import statements

--- app/src/main/java/io/r_a_d/radio/ActivityMain.java
+++ app/src/main/java/io/r_a_d/radio/ActivityMain.java
@@ -6,8 +6,6 @@ import android.graphics.Rect;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.os.SystemClock;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.res.ResourcesCompat;
